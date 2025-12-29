8afb84f741d30ecab7d75f39f39ac013a4492ac2
Dhruv Athaide <athaidedhruv@gmail.com>
2025-12-29 10:42:07 +0530

    Added authentication logic

--- android/app/src/main/kotlin/com/dhruvathaide/vanguard/MainActivity.kt
+++ android/app/src/main/kotlin/com/dhruvathaide/vanguard/MainActivity.kt
@@ -1,5 +1,25 @@
 package com.dhruvathaide.vanguard
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import android.view.WindowManager
 
-class MainActivity : FlutterActivity()
+class MainActivity: FlutterFragmentActivity() {
+    private val CHANNEL = "com.dhruvathaide.vanguard/security"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            if (call.method == "secure") {
+                window.addFlags(WindowManager.LayoutParams.FLAG_SECURE)
+                result.success(null)
+            } else if (call.method == "insecure") {
+                window.clearFlags(WindowManager.LayoutParams.FLAG_SECURE)
+                result.success(null)
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+}
